a2a05cf97bf58e1c21cea43d8ed347c88661c85d
Mithilesh Sharma <mithileshsharma539@gmail.com>
2023-10-14 14:27:09 +0530

    updating task 2 adding new features

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
-const getUrlFunc = (arr) => {
+const getUrlFunc = (arr, name) => {
   const getUrl = arr.map((e, i) => {
     return `<div class="item" draggable="true" ondragstart="drag(event)" key="${i}">
             <h4>${e}</h4>
             <div>
+            <i class="fa-solid fa-trash-can" onclick="deleteEvent('${e}','${name}')"></i>
             </div>
         </div>`;
   });
   return getUrl;
 };
+document
+  .getElementById("taskAdder")
+  .addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.key === "Enter") {
+      document.getElementById("addNewTask").click();
+    }
+  });
+
 function allowDrop(ev) {
   ev.preventDefault();
 }
+function deleteEvent(e, list) {
+  var arr;
+  if (list == "first") {
+    arr = JSON.parse(localStorage.getItem("new")) || [];
+    arr = arr.filter((text) => text != e);
+    localStorage.setItem("new", JSON.stringify(arr));
+    firstArrFetch();
+  } else if (list == "second") {
+    arr = JSON.parse(localStorage.getItem("inprogress")) || [];
+    arr = arr.filter((text) => text != e);
+    localStorage.setItem("inprogress", JSON.stringify(arr));
+    secondArrFetch();
+  } else {
+    arr = JSON.parse(localStorage.getItem("completed")) || [];
+    arr = arr.filter((text) => text != e);
+    localStorage.setItem("completed", JSON.stringify(arr));
+    thirdArrFetch();
+  }
+}
 function drag(event) {
-  console.log(event.srcElement.innerText ,"-"+event.srcElement.offsetParent.className);
   event.dataTransfer.setData(
     "text",
     `${event.srcElement.innerText} -${event.srcElement.offsetParent.className}`
@@ -21,21 +49,21 @@ function drag(event) {
 function firstArrFetch() {
   var storedNewTasks = JSON.parse(localStorage.getItem("new")) || [];
   const first = document.getElementById("first");
-  const getUrl = getUrlFunc(storedNewTasks);
+  const getUrl = getUrlFunc(storedNewTasks, "first");
   first.innerHTML = getUrl.join("");
 }
 function secondArrFetch() {
   var storedInProgressTasks =
     JSON.parse(localStorage.getItem("inprogress")) || [];
   const second = document.getElementById("second");
-  const getUrl = getUrlFunc(storedInProgressTasks);
+  const getUrl = getUrlFunc(storedInProgressTasks, "second");
   second.innerHTML = getUrl.join("");
 }
 function thirdArrFetch() {
   var storedCompletedTasks =
     JSON.parse(localStorage.getItem("completed")) || [];
   const third = document.getElementById("third");
-  const getUrl = getUrlFunc(storedCompletedTasks);
+  const getUrl = getUrlFunc(storedCompletedTasks, "third");
   third.innerHTML = getUrl.join("");
 }
 
